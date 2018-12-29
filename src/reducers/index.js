8b32ef20be782a1c2a03e0e4b03ebf307aa7d250
Kevin Boyle <papershack@gmail.com>
2018-12-28 19:15:50 -0800

    add fake reducer, update README

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,6 @@
 import { combineReducers } from 'redux';
 
-export default combineReducers({});
+export default combineReducers({
+//fake reducer to remove error message from console until real reducer implemented
+    fakeReducer: () => 'delete me'
+});
