92a6e6a72268468c47ab0139813d92214d78a1c8
Kevin Boyle <papershack@gmail.com>
2018-12-30 19:18:39 -0800

    add lodash library & functionality to memoizing fetchUsers action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
 export const fetchPosts = () => async function(dispatch) {
@@ -6,8 +7,20 @@ export const fetchPosts = () => async function(dispatch) {
         dispatch({ type: 'FETCH_POSTS', payload: response.data})
     };
 
-export const fetchUser = (id) => async dispatch => {
+
+// action creator fetchUser calls _fetchUser (lodash based function for making only one call to API for         fetching user)
+export const fetchUser = (id) => dispatch => {
+    _fetchUser(id, dispatch);
+
+
+    // const response = await jsonPlaceholder.get(`/users/${id}`);
+    // dispatch({ type: 'FETCH_USER', payload: response.data });
+};
+
+//private function to interact w/ Lodash library, we are memoizing our action creator
+
+const _fetchUser = _.memoize( async (id, dispatch) => {
     const response = await jsonPlaceholder.get(`/users/${id}`);
 
     dispatch({ type: 'FETCH_USER', payload: response.data });
-};
+});
